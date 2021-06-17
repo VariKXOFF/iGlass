11e028dbeb39b9fbb108cd441aae270bb0074dd0
VariKXOFF <varikxoff@yadnex.ru>
2021-06-18 00:46:43 +0300

    17.06.2021

--- api/db_goose.js
+++ api/db_goose.js
@@ -1,4 +1,6 @@
 const db = require("mongoose"),
+      user = "VariKX",
+      password = "qwerty123",
       uri = `mongodb+srv://${user}:${password}@cluster0.jlwsm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 db.connect(uri, {
