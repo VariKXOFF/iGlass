48ce14553eae69949993cab71b2b79aa7fc6d153
VariKXOFF <varikxoff@yadnex.ru>
2021-06-17 17:24:54 +0300

    17.06.2021

--- server/router.js
+++ server/router.js
@@ -3,6 +3,9 @@ const router = require("express").Router();
 router.get("/", (req, res) => {
 	res.render("index");
 });
+router.get("/order", (req, res) => {
+	res.render('order')
+})
 
 
 module.exports = router;
